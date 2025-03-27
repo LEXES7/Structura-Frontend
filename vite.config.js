7a7b83bc44a826ec4e2d3b16a0ec6a410a7d0f12
LEXES7 <sachinthabhashitha245@gmail.com>
2025-03-27 10:09:57 +0530

    fixed issues with tailwind config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import flowbiteReact from "flowbite-react/plugin/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import flowbiteReact from 'flowbite-react/plugin/vite';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
-})
+  plugins: [
+    react(),
+    flowbiteReact(),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  },
+});
